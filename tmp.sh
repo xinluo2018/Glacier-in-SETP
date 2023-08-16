6c5a98da61d336f8ebf1fe9925edbfedb3829a71
xin luo <xinluo_xin@163.com>
2023-08-16 15:55:24 +0800

    new commit

--- tmp.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-cd /home/xin/Developer-luo/Glacier-in-SETP
-
-python scripts/stat_dif_isat1.py
-python scripts/stat_dif_altimeter.py -dtype icesat-2
-python scripts/stat_dif_altimeter.py -dtype cryosat-2
-python ./scripts/stat_dif_dems.py
-
